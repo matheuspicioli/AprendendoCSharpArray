5c16df4d98bef1b5f6b141c4b8611c9cfa350482
Matheus Picioli <matheus.picioli98@gmail.com>
2018-12-30 16:42:49 -0200

    Curso C# 7 - arrays

---
+++ ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,144 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
+        public ListaDeContaCorrente(int capacidade = 5)
+        {
+            _itens = new ContaCorrente[capacidade];
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            // PROXIMAPOSICAO COMEÇA EM 0, POR ISSO SOMA +1
+            VerificarCapacidade(_proximaPosicao + 1);
+
+            //Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+
+            _itens[_proximaPosicao] = item;
+
+            _proximaPosicao++;
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if(_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+            if(novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            //Console.WriteLine($"Aumentando capacidade para {tamanhoNecessario}");
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            //Array.Copy(
+            //    sourceArray: _itens,
+            //    destinationArray: novoArray,
+            //    length: _itens.Length
+            //    );
+
+            for(int indice = 0; indice < _itens.Length; indice++)
+            {
+                //Console.WriteLine(".");
+                novoArray[indice] = _itens[indice];
+            }
+
+            _itens = novoArray;
+        }
+
+        public void Remover(ContaCorrente item)
+        {
+            int indiceItem = -1;
+
+            for(int i = 0; i < _proximaPosicao; i++)
+            {
+                ContaCorrente itemAtual = _itens[i];
+
+                if(itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+            if(indiceItem == -1)
+            {
+                return;
+            }
+
+            for(int i = indiceItem; i < _proximaPosicao - 1; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao--;
+            _itens[_proximaPosicao] = null;
+        }
+
+        public ContaCorrente GetItemNoIndice(int indice)
+        {
+            if(indice < 0 || indice >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        public void AdicionarVarios(params ContaCorrente[] itens)
+        {
+            foreach(ContaCorrente item in itens)
+            {
+                Adicionar(item);
+            }
+        }
+
+        public ContaCorrente this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
+        }
+
+        // params no indexador
+        //public ContaCorrente[] this[params int[] indices]
+        //{
+        //    get
+        //    {
+        //        ContaCorrente[] resultado = new ContaCorrente[indices.Length];
+
+        //        for(int i = 0; i < indices.Length; i++)
+        //        {
+        //            resultado[i] = GetItemNoIndice(indices[i]);
+        //        }
+
+        //        return resultado;
+        //    }
+        //}
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            ContaCorrente contaMatheus = new ContaCorrente(111, 123123);
+
+            lista.Adicionar(contaMatheus);
+            lista.Adicionar(new ContaCorrente(0001, 7687150));
+            lista.Adicionar(new ContaCorrente(0001, 7687151));
+            lista.Adicionar(new ContaCorrente(0001, 7687152));
+            lista.Adicionar(new ContaCorrente(0001, 7687153));
+            lista.Adicionar(new ContaCorrente(0001, 7687154));
+            lista.Adicionar(new ContaCorrente(0001, 7687155));
+            lista.Adicionar(new ContaCorrente(0001, 7687156));
+            lista.Adicionar(new ContaCorrente(0001, 7687157));
+            lista.Adicionar(new ContaCorrente(0001, 7687158));
+            lista.Adicionar(new ContaCorrente(0001, 7687159));
+
+            for(int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista[i];
+                Console.WriteLine($"Item na posição {i} = Conta {itemAtual.Numero}/{itemAtual.Agencia}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TestaArrayContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(0001, 7687150),
+                new ContaCorrente(0001, 7687199)
+            };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente conta = contas[indice];
+                Console.WriteLine($"Número da conta = {conta.Numero}");
+            }
+        }
+
+        static void TestaArrayInt()
+        {
+            int[] idades = null;
+            idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            int acumulador = 0;
+
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                acumulador += idades[indice];
+
+                Console.WriteLine($"Indíce na posição {indice} = {idades[indice]}");
+            }
+
+            int media = acumulador / idades.Length;
+
+            Console.WriteLine($"Média entre as idades = {media}");
+        }
+    }
+}
